b4028f871f8b11e3bf8091c6bc2839539ff36182
BBQ love 69 <74670055+zaw-creator@users.noreply.github.com>
2025-05-16 22:45:54 +0100

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1st
+            //for( int i =1;i <11;i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // Console.ReadLine();
+
+
+            //2
+            // 3 -> 1 + 2 + 3
+            //key take away cant return a value inside a void method
+
+
+            //Console.WriteLine("Enter any Numbers");
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+
+            //for (int i = 1;i <= num; i++)
+            //{
+            //    //Console.WriteLine(i);
+
+            //    sum = sum + i;
+
+
+            //}
+            //Console.WriteLine("Sum of the enter numbers " + sum);
+            //Console.ReadLine();
+
+            //3
+            //reverseastring();
+
+            //4
+            //key take away: use the for each with char to convert a string to char array
+            // also use the tostring to convert a char to string
+
+            //vowelcount();
+
+            //5
+            //largestnumber();
+
+            //6
+            //callingperson();
+
+            //7
+            //// refacotr - pass the arguments to the method to print the even numbers and this make the code more reusable
+            //int[] numbers = { 3, 6, 9, 12, 15 };
+            //printevennumbers(numbers);
+
+            //8
+            //Console.WriteLine("ENTER ANY WORDS");
+            //string input = Console.ReadLine();
+
+            //printeachchar(input);
+
+            //9 summary of array numbers
+            summaryofarray();
+
+            //10 create and use multiple instance of a class 
+        
+             callingperson();
+            callingperson();
+            callingperson();
+            //:)) maybe this is the real solution for this task
+
+
+
+
+
+
+
+
+
+
+
+        }
+        static void reverseastring()
+        {
+            Console.WriteLine("Enter any words to reverse!!");
+            string inputfromuser = Console.ReadLine();
+            char[] reverse = inputfromuser.ToCharArray();
+            Array.Reverse(reverse);
+
+            Console.WriteLine("reverse string : " + new string(reverse));
+
+
+        }
+
+        static void vowelcount()
+        {
+           Console.WriteLine("Enter any words to count vowels!!");
+            string userinput = Console.ReadLine().ToLower();
+            string[] vowels = { "a", "e", "i", "o", "u" };
+
+
+            int count = 0;
+
+            foreach (string vowel in vowels)
+            {
+                foreach(char i in userinput)
+                {
+                    if(i.ToString() == vowel)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Vowels count : "+ count);
+
+
+
+        }
+        static void largestnumber()
+        {
+            int[] nums = { 4, 7, 1, 12, 9 };
+
+            int max = 0;
+
+            foreach (int num in nums)
+            {
+                if(num > max)
+                {
+                    max = num;
+
+                   
+                }
+               
+            }
+            Console.WriteLine("largest number is " + max);
+
+
+
+
+
+        }
+
+    
+
+        public class Person {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public void Display()
+
+            //in here have to use the public void because This means Display() needs to be an instance method, and not static, so it can access the instance's data (Name, Age).
+            {
+                Console.WriteLine($"Name : {Name}, Age: {Age}");
+            }
+
+
+
+        }
+
+
+        // is a basic step
+
+        //static void callingperson()
+        //{
+        //    Person p = new Person();
+        //    p.Name = "Alice";
+        //    p.Age = 24;
+        //    p.Display();
+        //}
+
+        //refactor
+        // can add the authentication like to only accept the string and then to add only the int and not to add the age more than 100 not less than 0 :)
+
+        static void callingperson()
+        {
+            Person p = new Person();
+            Console.WriteLine("enter the person name ");
+            p.Name = Console.ReadLine();
+            Console.WriteLine("enter the person age ");
+            p.Age = int.Parse(Console.ReadLine());
+            p.Display();
+        }
+
+        static void printevennumbers(int[] numbers)
+        {
+            
+
+            foreach( int num in numbers)
+            {
+                if (num%2==0)
+                {
+                    
+                    Console.WriteLine(num);
+                    
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void printeachchar(string input)
+        {
+            foreach ( char i in input)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void summaryofarray()
+        {
+            int[] numbers = { 2, 4, 6, 8 };
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+        }
+
+    }
+}
